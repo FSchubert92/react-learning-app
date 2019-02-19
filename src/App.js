372f98acf1c54d8686000322695fe53bc0bca5ee
Florian Schubert <flo.schubert@me.com>
2019-02-19 17:30:49 +0100

    Add all

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import styled from 'styled-components'
+const StyledText = styled.h1`
+  font-size: 3em;
+  color: hotpink;
+`
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <StyledText>Hello World</StyledText>
   }
 }
 
-export default App;
+export default App
